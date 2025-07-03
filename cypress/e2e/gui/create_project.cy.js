faa1bc1dd4363e21235894b7e4f8ff47cc29c531
rlucasorsi <rlucasorsi@gmail.com>
2025-07-03 11:34:31 -0300

    test: add cy.session support to improve test speed

--- cypress/e2e/gui/create_project.cy.js
+++ cypress/e2e/gui/create_project.cy.js
@@ -1,19 +1,27 @@
-import { faker } from '@faker-js/faker'
+import { faker } from '@faker-js/faker';
 
 describe('Create Projects', () => {
-    beforeEach(() => {
-        cy.login();
-    });
-    it.only('Successfully', () => {
-        const project = {
-            name: `project-${faker.datatype.uuid()}`,
-            description: faker.random.words(5)
-        }   
-        
-        cy.gui_createProject(project)
+  beforeEach(() => {
+    const user = Cypress.env('user_name');
+    const password = Cypress.env('user_password');
+    const options = { cacheSession: true };
+    
+    cy.login(user, password, options);
+  });
 
-        cy.url().should('be.equal', `${Cypress.config('baseUrl')}/${Cypress.env('user_name')}/${project.name}`)
-        cy.contains(project.name).should('be.visible')
-        cy.contains(project.description).should('be.visible')
-    });
-}); 
+  it.only('Successfully', () => {
+    const project = {
+      name: `project-${faker.datatype.uuid()}`,
+      description: faker.random.words(5),
+    };
+
+    cy.gui_createProject(project);
+
+    cy.url().should(
+      'be.equal',
+      `${Cypress.config('baseUrl')}/${Cypress.env('user_name')}/${project.name}`
+    );
+    cy.contains(project.name).should('be.visible');
+    cy.contains(project.description).should('be.visible');
+  });
+});
--- cypress/e2e/gui/login.cy.js
+++ cypress/e2e/gui/login.cy.js
@@ -1,7 +1,12 @@
 describe('Login', () => {
   
-  it('Login com sucesso', () => {
-    cy.login();
-    cy.get('[data-qa-selector="welcome_title_content"]').should('be.visible');
+  it('successfully', () => {
+    const user = Cypress.env('user_name')
+    const password = Cypress.env('user_password')
+    const options = {cacheSession: false}
+
+    cy.login(user, password, options);
+
+    cy.get('.qa-user-avatar').should('be.visible');
   });
 });
