d896d7ae0f235fe95512f70b350c0e2961942116
rlucasorsi <rlucasorsi@gmail.com>
2025-07-04 14:58:12 -0300

    test: add e2e tests for issue creation

---
+++ cypress/e2e/gui/create_issue.cy.js
@@ -0,0 +1,21 @@
+import { faker } from '@faker-js/faker';
+
+describe('Create issue', () => {
+  const issue = {
+    title: `issue-${faker.datatype.uuid()}`,
+    description: faker.random.words(5),
+    project: {
+      name: `project-${faker.datatype.uuid()}`,
+      description: faker.random.words(5),
+    },
+  };
+  beforeEach(() => {
+    cy.login();
+    cy.gui_createProject(issue.project);
+  });
+  it('Successfully', () => {
+    cy.gui_createIssue(issue);
+
+    cy.get('.issue-details').should('contain', issue.title).and('contain', issue.description);
+  });
+});
